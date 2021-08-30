3aa1fa1b146d35bc4c6c595896f82577232b0c37
Gaston <70915311+Hyouka21@users.noreply.github.com>
2021-08-30 17:57:22 -0300

    segundo

--- app/src/main/java/com/sosa/practicon2sosagaston/MensajeTexto.java
+++ app/src/main/java/com/sosa/practicon2sosagaston/MensajeTexto.java
@@ -11,36 +11,51 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 
 public class MensajeTexto extends Service {
-    private final int TIEMPO = 9000;
+
     public MensajeTexto(){
     }
-    public void leer(){
-        int contador=0;
-        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
-        if (cursor.moveToFirst()) {
-            do {
-                String msg = "";
 
-                msg += " " + cursor.getColumnName(0) + ":" + cursor.getString(0);
-                msg += " " + cursor.getColumnName(2) + ":" + cursor.getString(2);
-                msg += " " + cursor.getColumnName(4) + ":" + cursor.getString(4);
-                msg += " " + cursor.getColumnName(12) + ":" + cursor.getString(12);
-
-
-                contador++;
-                Log.d("mensaje",msg);
-                if(contador>4){break;}
-            } while (cursor.moveToNext());
-        } else {
-            Log.d("mensaje","No hay mensajes");
-        }
-    }
     @Override
     public void onCreate() {
         super.onCreate();
 
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
 
-                    leer();
+                    int contador = 0;
+                    Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+                    if (cursor.moveToFirst()) {
+                        do {
+                            String msg = "";
+
+                            msg += " " + cursor.getColumnName(0) + ":" + cursor.getString(0);
+                            msg += " " + cursor.getColumnName(2) + ":" + cursor.getString(2);
+                            msg += " " + cursor.getColumnName(4) + ":" + cursor.getString(4);
+                            msg += " " + cursor.getColumnName(12) + ":" + cursor.getString(12);
+
+
+                            contador++;
+                            Log.d("mensaje", msg);
+                            if (contador > 4) {
+                                break;
+                            }
+                        } while (cursor.moveToNext());
+                    } else {
+                        Log.d("mensaje", "No hay mensajes");
+                    }
+                    try {
+                        Thread.sleep(9000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    cursor.close();
+                }
+
+            }
+        });
+        th.start();
 
 
 
@@ -50,4 +65,8 @@ public class MensajeTexto extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+
+
+
 }
